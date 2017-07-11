7d4560b9931ff6654989de489e0c0c883bce4c0b
Logan Cox <lmc@ou.edu>
2017-07-11 14:39:49 -0500

    Clean up and comment

--- lib/DropboxTransfer.php
+++ lib/DropboxTransfer.php
@@ -1,17 +1,35 @@
 <?php
 
-
-
+/**
+ * Minimal Dropbox file transfer class supporting just enough of the v2 HTTP API
+ * to do chunked file transfers with Fine Uploader.
+ */
 class DropboxTransfer {
+
+  /**
+   * @param $auth
+   * A Dropbox oauth token
+   * @param $request
+   * A FineUploader traditional endpoint request
+   * @param $sessionid
+   * A dropbox Session id
+   * @param $gzclient
+   * A Guzzle HTTP client
+   * @param $chunk_path
+   * Path to a chunk that we'll transfer
+   * @param $remote_file
+   * Dropbox path where we're trying to create a file.
+   */
+
 
   function __construct($auth, $request,  $sessionid, $gzclient, $chunk_path, $remote_file) {
     $this->auth = $auth;
-    $this->uuid = $request["qquuid"];
-    $this->offset = (int)$request["qqpartbyteoffset"];
+    $this->uuid = $request["qquuid"]; // transfer uuid assigned by fine uploader
+    $this->offset = (int)$request["qqpartbyteoffset"]; // amount of data already sent
     $this->remote_file =  $remote_file;
-    $this->totalparts = (int)$request["qqtotalparts"];
-    $this->totalfilesize = (int)$request["qqtotalfilesize"];
-    $this->partindex = (int)$request["qqpartindex"];
+    $this->totalparts = (int)$request["qqtotalparts"]; // total number of chunks
+    $this->totalfilesize = (int)$request["qqtotalfilesize"]; // total data
+    $this->partindex = (int)$request["qqpartindex"]; // index of this chunk
     $this->chunk_path = $chunk_path;
     $this->chunk_size = filesize($chunk_path);
     $this->chunk = fopen($chunk_path, "r");
@@ -20,6 +38,16 @@ class DropboxTransfer {
   }
 
 
+  /**
+   * Perform a Dropbox file transfer command with the v2 HTTP API.
+   *
+   * See https://www.dropbox.com/developers/documentation/http/documentation
+   *
+   *  @param array $api_args
+   *    The arguments required an Dropbox command.
+   *  @param string $target
+   *    An API endpoint URI for a dropbox command.
+   */
   function doTransfer( $api_args, $target) {
 
     $request = $this->gzclient->createRequest('POST', $target);
@@ -52,12 +80,20 @@ class DropboxTransfer {
   }
 
 
+  /**
+   * Start a transfer session
+   */
   function start() {
-    $api_args = ["close"=> false];
+    $api_args = [
+      "close"=> false
+    ];
     $target = 'https://content.dropboxapi.com/2/files/upload_session/start';
     return $this->doTransfer( $api_args, $target);
   }
 
+  /**
+   * Append a chunk to an open transfer session
+   */
   function appendv2() {
     $api_args = [
       "close"=> false,
@@ -66,13 +102,14 @@ class DropboxTransfer {
         "offset" => $this->offset
       ]
     ];
-
     $target= 'https://content.dropboxapi.com/2/files/upload_session/append_v2';
 
     return $this->doTransfer($api_args, $target);
   }
 
-
+  /**
+   * Send final chunk and finalize transfer
+   */
   function finish() {
     $api_args = [
       "cursor" => [
@@ -90,7 +127,9 @@ class DropboxTransfer {
     return $this->doTransfer($api_args, $target);
   }
 
-
+  /**
+   * Do a simple file upload for small files
+   */
   function upload() {
     $api_args = [
       "path"=> "/".$this->remote_file ,
@@ -101,5 +140,4 @@ class DropboxTransfer {
     $target='https://content.dropboxapi.com/2/files/upload';
     return $this->doTransfer($api_args, $target);
   }
-
 }
